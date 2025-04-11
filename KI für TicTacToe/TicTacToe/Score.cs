8ecffd724809c219ac95092f1cae82c588167937
Julius Steinbach <94620733+JuliusStei@users.noreply.github.com>
2025-04-11 14:05:02 +0200

    Code

---
+++ KI für TicTacToe/TicTacToe/Score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Score
+    {
+        public int WinCountPlayer1 { get; set; } = 0;
+        public int WinCountPlayer2 { get; set; } = 0;
+        public int LossCountPlayer1 { get; set; } = 0;
+        public int LossCountPlayer2 { get; set; } = 0;
+        public int DrawCount { get; set; } = 0;
+        public int GamesPlayedCount { get; set; } = 0;
+        public Score() { }
+        public void Reset()
+        {
+            WinCountPlayer1 = 0;
+            WinCountPlayer2 = 0;
+            LossCountPlayer1 = 0;
+            LossCountPlayer2 = 0;
+            DrawCount = 0;
+            GamesPlayedCount = 0;
+        }
+    }
+}
